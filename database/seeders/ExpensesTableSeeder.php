b7014410d1c6fa564a9314e765f6e224a82b93f1
Jeremy-Pacheco <183743767+Jeremy-Pacheco@users.noreply.github.com>
2025-10-16 19:33:09 +0100

    update:seeders

--- database/seeders/ExpensesTableSeeder.php
+++ database/seeders/ExpensesTableSeeder.php
@@ -13,22 +13,22 @@ class ExpensesTableSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        DB::table('expenses')->insert([
-            [
-                'user_id' => 1,
-                'category'=>'Leisure', 
-                'mount'=>9.99, 
-                'description'=>'Movie ticket', 
-                'purchase_date'=>'2025-10-13'
-            ],
-            [
-                'user_id' => 1,
-                'category'=>'Transport', 
-                'mount'=>1.60, 
-                'description'=>'Transport ticket', 
-                'purchase_date'=>'2025-10-09'
-            ],
-        ]);
+    {       //!Activate it when you create a user and run the seeder.
+        // DB::table('expenses')->insert([
+        //     [
+        //         'user_id' => 1,
+        //         'category'=>'Leisure', 
+        //         'mount'=>9.99, 
+        //         'description'=>'Movie ticket', 
+        //         'purchase_date'=>'2025-10-13'
+        //     ],
+        //     [
+        //         'user_id' => 1,
+        //         'category'=>'Transport', 
+        //         'mount'=>1.60, 
+        //         'description'=>'Transport ticket', 
+        //         'purchase_date'=>'2025-10-09'
+        //     ],
+        // ]);
     }
 }
